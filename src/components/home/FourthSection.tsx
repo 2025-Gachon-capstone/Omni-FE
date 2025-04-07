08273e32433b9a5af3bd94e39589fdda5e207b87
odukong <ohbin1017@gmail.com>
2025-04-07 19:35:38 +0900

    feat : FourthSection 컴포넌트 구현
    - 랜딩페이지의 네 번째 섹션인 FourthSection을 구현하였습니다.
    - 해당 컴포넌트에 사용되는 카드 컴포넌트 (StepSection)을 구현하였습니다.
    
    ref : #1

---
+++ src/components/home/FourthSection.tsx
@@ -0,0 +1,76 @@
+/** @jsxImportSource @emotion/react */
+import { css } from '@emotion/react';
+import theme from '../../styles/theme';
+import { StepSection } from './StepSection';
+import { data } from '../../data/StepData';
+
+export const FourthSection = () => {
+  return (
+    <div
+      css={css`
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        align-items: center;
+        gap: 3.75rem;
+        padding: 10% 5% 15% 5%;
+        box-sizing: border-box;
+      `}
+    >
+      <div
+        css={css`
+          display: flex;
+          flex-direction: column;
+          align-items: center;
+          gap: 1rem;
+        `}
+      >
+        <div
+          css={css`
+            width: 2.5rem;
+            height: 2.5rem;
+            border-radius: 50%;
+            background-color: ${theme.color.main};
+          `}
+        ></div>
+        <div
+          css={css`
+            font-size: 1.5rem;
+            font-weight: 600;
+            color: ${theme.color.main};
+          `}
+        >
+          Service step
+        </div>
+        <div
+          css={css`
+            font-size: 2rem;
+            font-weight: 800;
+          `}
+        >
+          서비스 이용방법
+        </div>
+      </div>
+      {/** 카드섹션 */}
+      <div
+        css={css`
+          display: flex;
+          justify-content: center;
+          flex-wrap: wrap;
+          gap: 1.5rem;
+        `}
+      >
+        {data.map((el) => {
+          return (
+            <StepSection
+              key={el.step}
+              step={el.step}
+              title={el.title}
+              description={el.description}
+            />
+          );
+        })}
+      </div>
+    </div>
+  );
+};
---
+++ src/components/home/StepSection.tsx
@@ -0,0 +1,60 @@
+/** @jsxImportSource @emotion/react */
+import { css } from '@emotion/react';
+import theme from '../../styles/theme';
+
+type StepInfo = {
+  step: number; // 단계
+  title: string; // step별 제목
+  description: string; // step별 설명
+};
+
+export const StepSection = ({ step, title, description }: StepInfo) => {
+  return (
+    <div
+      css={css`
+        max-width: 19rem;
+        padding: 0.5rem 1rem 3.8rem 1rem;
+        border: 2px solid #d9d9d9;
+        border-radius: 12px;
+      `}
+    >
+      <div
+        css={css`
+          color: #d9d9d9;
+          font-weight: 800;
+          font-size: 3rem;
+        `}
+      >
+        0{step}
+      </div>
+      <div
+        css={css`
+          display: flex;
+          flex-direction: column;
+          align-items: center;
+          gap: 1.5rem;
+          margin: 3rem auto;
+          padding: 0 2rem;
+        `}
+      >
+        <div
+          css={css`
+            font-size: 2rem;
+            font-weight: 700;
+            color: ${theme.color.main};
+          `}
+        >
+          {title}
+        </div>
+        <div
+          css={css`
+            font-size: 1.25rem;
+            color: #505050;
+          `}
+        >
+          {description}
+        </div>
+      </div>
+    </div>
+  );
+};
