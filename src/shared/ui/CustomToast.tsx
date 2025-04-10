48bc1ea13ef4f0edfb9c631547fafccf088dc26d
odukong <ohbin1017@gmail.com>
2025-04-10 23:02:52 +0900

    design : 공통 토스트컴포넌트 구현
    - 각 상황에 맞는 토스트 스타일을 사용할 수 있도록 CustomToast 컴포넌트를 구현하였습니다.
    
    ref : #3

---
+++ src/shared/ui/CustomToast.tsx
@@ -0,0 +1,65 @@
+import { ToastContainer } from 'react-toastify';
+import styled from '@emotion/styled';
+import theme from '../styles/theme';
+
+export const CustomToast = () => {
+  return (
+    <StyledToastConatiner
+      position="top-right"
+      autoClose={1000}
+      limit={1}
+      hideProgressBar={true}
+      theme="colored"
+      closeButton={false}
+    />
+  );
+};
+
+export default CustomToast;
+
+const StyledToastConatiner = styled(ToastContainer)`
+  .Toastify__toast {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    width: auto;
+    height: 1.6875rem;
+    top: 1rem;
+    right: 1rem;
+    padding: 2rem 1.5rem;
+    border-radius: 0.5rem;
+
+    font-size: 1rem;
+  }
+
+  .Toastify__toast-body {
+    margin: 0;
+    padding: 0;
+  }
+
+  .Toastify__toast--info {
+    // info 토스트 디자인
+  }
+
+  .Toastify__toast--error {
+    // error 토스트 디자인
+    color: white;
+    background: ${theme.color.red};
+    svg {
+      fill: white !important;
+    }
+  }
+
+  .Toastify__toast--success {
+    color: white;
+    background: ${theme.color.main};
+
+    svg {
+      fill: white !important;
+    }
+  }
+
+  .Toastify__toast--warning {
+    // warning 토스트 디자인
+  }
+`;
