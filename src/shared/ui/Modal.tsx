7f2cd8598d2c0b5d2c40ce2e47421bfda76d13e1
odukong <ohbin1017@gmail.com>
2025-04-10 23:10:06 +0900

    feat : 공통 Modal 컴포넌트 구현
    - 공통으로 사용될 Modal 컴포넌트를 구현하였습니다.
    - 모달창 아이콘, 모달 내용, 모달 버튼(최대 2개) 요소를 커스텀하여 사용할 수 있습니다. (ex. Join Page  참조하여 사용)
    
    ref : 3

---
+++ src/shared/ui/Modal.tsx
@@ -0,0 +1,122 @@
+import styled from '@emotion/styled';
+import theme from '../styles/theme';
+import { useEffect } from 'react';
+
+type ModalButton = {
+  text: string;
+  onClick: () => void;
+  bgColor?: string; // 버튼 배경색 (선택)
+  textColor?: string; // 버튼 텍스트 색 (선택)
+  border?: string; // 버튼 테두리 색 (선택)
+};
+
+type ModalProps = {
+  icon?: React.ReactNode;
+  children: React.ReactNode;
+  buttons?: ModalButton[]; // 최대 2개
+};
+
+const Modal = ({ icon, children, buttons = [] }: ModalProps) => {
+  // body 스크롤 막기
+  useEffect(() => {
+    document.body.style.overflow = 'hidden';
+    return () => {
+      document.body.style.overflow = 'auto';
+    };
+  }, []);
+
+  return (
+    <Container>
+      <Content>
+        <Header>{icon ?? <Space />}</Header>
+        <Description>{children}</Description>
+        <ButtonWrapper>
+          {buttons.map((btn, idx) => (
+            <ModalButton
+              key={idx}
+              onClick={btn.onClick}
+              bgColor={btn.bgColor}
+              textColor={btn.textColor}
+              border={btn.border}
+            >
+              {btn.text}
+            </ModalButton>
+          ))}
+        </ButtonWrapper>
+      </Content>
+    </Container>
+  );
+};
+
+export default Modal;
+
+const Container = styled.div`
+  position: fixed;
+  z-index: 10;
+  width: 100vw;
+  height: 100vh;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  background-color: rgba(0, 0, 0, 0.4);
+`;
+
+const Content = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  gap: 2rem;
+
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+
+  width: 90vw;
+  max-width: 16rem;
+  padding: 1.19rem 2.69rem;
+
+  background-color: white;
+  border-radius: 0.25rem;
+`;
+
+const Header = styled.div`
+  margin: 0 auto;
+`;
+
+const ButtonWrapper = styled.div`
+  display: flex;
+  justify-content: center;
+  gap: 0.75rem;
+  width: 100%;
+`;
+
+const ModalButton = styled.button<{ bgColor?: string; textColor?: string; border?: string }>`
+  padding: 0.5rem 2rem;
+  border-radius: 0.25rem;
+
+  background-color: ${(props) => props.bgColor || `${theme.color.main}`};
+  color: ${(props) => props.textColor || '#fff'};
+  border: ${(props) => props.border || 'none'};
+  font-size: 1rem;
+
+  cursor: pointer;
+  transition: all 0.2s ease;
+
+  &:hover {
+    opacity: 0.85;
+  }
+`;
+
+const Description = styled.div`
+  text-align: center;
+  font-size: 1.25rem;
+  font-weight: 600;
+  color: black;
+`;
+
+const Space = styled.div`
+  width: 1rem;
+  height: 1rem;
+`;
