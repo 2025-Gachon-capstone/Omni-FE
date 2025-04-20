a0beedebe25b72e88ff8042685e417d97c050a9d
loading1031 <tjdans1031@gmail.com>
2025-04-20 22:55:17 +0900

    refactor: store/index.ts 파일 분리 - index.ts는 export만 선언 - headerStore.ts에 기능 이전
    ref: #5

--- src/shared/store/index.ts
+++ src/shared/store/index.ts
@@ -1,12 +1,2 @@
 // index.ts
-import { create } from 'zustand';
-
-interface HeaderState {
-  headerHeight: number;
-  setHeaderHeight: (height: number) => void;
-}
-
-export const useHeaderStore = create<HeaderState>((set) => ({
-  headerHeight: 0,
-  setHeaderHeight: (height) => set({ headerHeight: height }),
-}));
+export { useHeaderStore } from './headerStore'; 
