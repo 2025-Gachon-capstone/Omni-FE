b791f761f33362fffc6191318d5fc3fea37aaf4b
odukong <ohbin1017@gmail.com>
2025-04-21 19:47:42 +0900

    feat : 로딩 컴포넌트 구현
    ref : #10

---
+++ src/pages/Loading.tsx
@@ -0,0 +1,44 @@
+import styled from '@emotion/styled';
+import theme from '../shared/styles/theme';
+
+const Loading = () => {
+  return (
+    <SpinnerWrapper>
+      <Spinner />
+    </SpinnerWrapper>
+  );
+};
+
+export default Loading;
+
+const SpinnerWrapper = styled.div`
+  z-index: 1000;
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  color: #3e3e3e;
+  background-color: rgba(255, 255, 255, 0.4);
+`;
+
+const Spinner = styled.div`
+  border: 4px solid #f3f3f3;
+  border-top: 4px solid ${theme.color.main};
+  border-radius: 50%;
+  width: 50px;
+  height: 50px;
+  animation: spin 1s linear infinite;
+
+  @keyframes spin {
+    0% {
+      transform: rotate(0deg);
+    }
+    100% {
+      transform: rotate(360deg);
+    }
+  }
+`;
