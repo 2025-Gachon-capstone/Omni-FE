a70ef50ebb26a69248e6df5cd66ce1a1b05a897e
odukong <ohbin1017@gmail.com>
2025-07-02 15:33:12 +0900

    feat : 카드추가 모달 UI 추가
    - 새로운 카드 추가를 위해 비밀번호를 입력받는 Modal 추가
    
    ref : #36

--- src/pages/UserMyCard.tsx
+++ src/pages/UserMyCard.tsx
@@ -1,43 +1,96 @@
-import { useState } from 'react';
+import { useCallback, useState } from 'react';
 import styled from '@emotion/styled';
 import useDevice from '../shared/hooks/useDevice';
-import { Title } from '../shared/ui';
-import Card from '../shared/assets/img/card.svg';
-import { PasswordAuth } from '../features/user/myCard/ui/PasswordAuth';
-import { MyCardInfo } from '../features/user/myCard/ui/MyCardInfo';
+import { Button, Title } from '../shared/ui';
+import Modal from '../shared/ui/Modal';
+import theme from '../shared/styles/theme';
+import { toast } from 'react-toastify';
 
 const UserMyCard = () => {
   const { isMobile } = useDevice();
+  const [isOpen, setIsOpen] = useState(false); // 카드추가 모달창
   const [cardPassword, setCardPassword] = useState(''); // 카드비밀번호
-  const [isMine, setIsMine] = useState(-1); // 카드비밀번호 (-1: 초기, 0: 인증실패, 1: 인증성공)
+  const [isCardPasswordValid, setIsCardPasswordValid] = useState(-1); // 카드 비밀번호 형식 (-1: 초기, 0: 형식X, 1: 형식O)
+
+  // 모달창 컨텐츠 초기화
+  const resetContent = () => {
+    setCardPassword('');
+    setIsCardPasswordValid(-1);
+    setIsOpen(false);
+  };
+
+  /** ------------- 카드 생성하기 API --------------- */
+  const createCard = useCallback(() => {
+    if (/^\d{4}$/.test(cardPassword)) {
+      setIsCardPasswordValid(1);
+    } else {
+      setIsCardPasswordValid(0);
+    }
+
+    if (isCardPasswordValid) {
+      // 카드 생성 API
+      // 성공, 실패
+      let result = 1;
+      if (result) {
+        toast.success('카드가 성공적으로 추가되었습니다.');
+      } else {
+        toast.error('카드 추가에 실패했습니다.');
+      }
+    }
+  }, [cardPassword, isCardPasswordValid]);
 
   return (
-    <Container>
-      <MyCardContainer isMobile={isMobile}>
-        <div className="title">
-          <Title main="MY카드" sub="내 카드 정보를 확인해보세요." />
-        </div>
-        <ImgBox>
-          <img src={Card} width={180} />
-        </ImgBox>
-        {/** 비밀번호 불일치 >> PasswordAuth
-         *   비밀번호 일치   >> MyCardInfo
-         */}
-        {isMine !== 1 ? (
-          <PasswordAuth
-            cardPassword={cardPassword}
-            isMine={isMine}
-            handleCardPassword={(e) => {
-              setCardPassword(e.target.value);
-              setIsMine(-1);
-            }}
-            handleIsMine={setIsMine}
-          />
-        ) : (
-          <MyCardInfo />
-        )}
-      </MyCardContainer>
-    </Container>
+    <>
+      <Container>
+        <MyCardContainer isMobile={isMobile}>
+          <div className="title">
+            <Title main="MY카드" sub="내 카드 정보를 확인하고 새로운 카드를 추가하세요." />
+          </div>
+          <div className="new-button">
+            <Button width="8.5rem" textSize="1rem" onClick={() => setIsOpen(true)}>
+              카드 추가하기
+            </Button>
+          </div>
+          <div className="content"></div>
+        </MyCardContainer>
+      </Container>
+      {isOpen && (
+        <Modal
+          buttons={[
+            {
+              text: '취소하기',
+              onClick: () => resetContent(),
+              bgColor: 'white',
+              textColor: '#7C7F86',
+              border: '1px solid #7C7F86',
+            },
+            {
+              text: '추가하기',
+              onClick: () => {
+                createCard();
+                resetContent();
+              },
+              bgColor: theme.color.main,
+              textColor: 'white',
+            },
+          ]}
+        >
+          <ModalContent>
+            <div>새 카드의 비밀번호를 입력해주세요.</div>
+            <input
+              placeholder="숫자 4자리"
+              type="password"
+              maxLength={4}
+              value={cardPassword}
+              onChange={(e) => setCardPassword(e.target.value)}
+            />
+            {isCardPasswordValid == 0 && (
+              <div className="error-phrase">비밀번호 형식에 맞게 입력해주세요.</div>
+            )}
+          </ModalContent>
+        </Modal>
+      )}
+    </>
   );
 };
 
@@ -62,8 +115,33 @@ const MyCardContainer = styled.div<{ isMobile: boolean }>`
   .title {
     margin-right: auto;
   }
+  .new-button {
+    align-self: flex-end;
+  }
 `;
 
-const ImgBox = styled.div`
-  margin: 0 auto;
+const ModalContent = styled.div`
+  margin-top: -1.5rem;
+
+  display: flex;
+  flex-direction: column;
+  gap: 1.5rem;
+
+  input {
+    outline: none;
+    border: none;
+    width: 16rem;
+    padding: 0.5rem 0.75rem;
+    background-color: #f4f4f4;
+    border-radius: 0.25rem;
+  }
+
+  .error-phrase {
+    font-size: 0.75rem;
+    font-weight: 300;
+    color: ${theme.color.red};
+
+    align-self: flex-start;
+    margin: -0.5rem 0 0 0.5rem;
+  }
 `;
