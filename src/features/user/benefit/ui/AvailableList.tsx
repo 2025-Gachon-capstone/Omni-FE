9108765d1e255976b3e4db1fa12361b99e742b4f
odukong <ohbin1017@gmail.com>
2025-07-20 19:13:08 +0900

    fix : 가용혜택 데이터 key 수정

--- src/features/user/benefit/ui/AvailableList.tsx
+++ src/features/user/benefit/ui/AvailableList.tsx
@@ -17,7 +17,7 @@ export const AvailableList = ({ data }: { data: Benefit[] }) => {
       </TitleWrap>
       <Content>
         {data.map((el) => {
-          return <BenefitItem key={el.cardBenefitId}>{el.title}</BenefitItem>;
+          return <BenefitItem key={el.benefitId}>{el.title}</BenefitItem>;
         })}
       </Content>
     </Container>
