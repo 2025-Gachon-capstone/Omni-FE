c4c80171b7df0e4f2af0101b6fa34c60b52a95d5
odukong <ohbin1017@gmail.com>
2025-08-08 16:26:05 +0900

    feat : 맞춤 마케팅 비율 데이터 핸들링
    - 슬라이더와 입력 필드에 비율이 입력됨에 따라 useCustomBenefit에 값을 저장하도록 구현.
    
    ref : #41

--- src/features/sponsor/report/ui/CustomRatioSection.tsx
+++ src/features/sponsor/report/ui/CustomRatioSection.tsx
@@ -1,7 +1,31 @@
 import styled from '@emotion/styled';
+import { useState } from 'react';
 import theme from '../../../../shared/styles/theme';
+import { useCustomBenefit } from '../model/useCustomBenefit';
 
 export const CustomRatioSection = () => {
+  const { customState, setRatio } = useCustomBenefit((state) => state);
+  const [value, setValue] = useState(100 - customState.reorderRatio * 100); // 첫 구매 기준
+
+  /** --------------- 첫구매자, 재구매자 특성 반영 비율 설정 ------------------ */
+  // 첫 구매
+  const handleFirstChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    let value = Number(e.target.value);
+    if (isNaN(value)) return;
+    value = Math.max(0, Math.min(100, value));
+    setValue(value);
+    setRatio((100 - value) / 100);
+  };
+  // 재구매
+  const handleReorderChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    let reorderValue = Number(e.target.value);
+    if (isNaN(reorderValue)) return;
+    reorderValue = Math.max(0, Math.min(100, reorderValue));
+    const newFirstValue = 100 - reorderValue;
+    setValue(newFirstValue);
+    setRatio(reorderValue / 100);
+  };
+
   return (
     <Wrapper>
       {/** 비율 값 컨테이너 */}
@@ -10,19 +34,19 @@ export const CustomRatioSection = () => {
         <RatioBox>
           <Name>첫 구매</Name>
           <ValueBox>
-            <Value type="number" />%
+            <Value type="number" value={value} onChange={handleFirstChange} />%
           </ValueBox>
         </RatioBox>
         {/** (재구매) 비율 */}
         <RatioBox>
           <Name>재구매</Name>
           <ValueBox>
-            <Value type="number" />%
+            <Value type="number" value={100 - value} onChange={handleReorderChange} />%
           </ValueBox>
         </RatioBox>
       </RatiosWrapper>
       {/** 비율 슬라이더 */}
-      <RatioSlider type="range" />
+      <RatioSlider type="range" min="0" max="100" value={value} onChange={handleFirstChange} />
     </Wrapper>
   );
 };
