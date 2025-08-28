d35d87cadfc98fade9df8b438ae989d48f38daaf
odukong <ohbin1017@gmail.com>
2025-08-28 19:55:30 +0900

    feat : 상품 통계 조회 API 연동 (#43)

--- src/features/sponsor/report/api/useGetProducts.ts
+++ src/features/sponsor/report/api/useGetProducts.ts
@@ -1,6 +1,7 @@
 import { useState } from 'react';
 import { toast } from 'react-toastify';
 import { privateAxios } from '../../../../app/customAxios';
+import { StatisticsData } from '../type/StatisticsType';
 
 interface Product {
   productId: number;
@@ -24,5 +25,30 @@ export const useGetProducts = () => {
     }
   };
 
-  return { getProductsList, isLoading };
+  // (GET) 상품 통계리포트 조회
+  const getProductsReport = async ({
+    productId,
+  }: {
+    productId: number;
+  }): Promise<StatisticsData> => {
+    setIsLoading(true);
+    try {
+      const result = await privateAxios.get(`/flask/v1/products/${productId}/statistics`);
+      return result.data.result;
+    } catch (err: any) {
+      toast.error('데이터를 불러오는데 실패했습니다.');
+      return {
+        reordered: [],
+        orderHour: [],
+        orderDow: [],
+        relatedProduct: [],
+        report: '정보없음',
+        period: { max: '0000.00.00', min: '0000.00.00' },
+      };
+    } finally {
+      setIsLoading(false);
+    }
+  };
+
+  return { getProductsList, getProductsReport, isLoading };
 };
--- src/features/sponsor/report/ui/ProductReport.tsx
+++ src/features/sponsor/report/ui/ProductReport.tsx
@@ -4,68 +4,15 @@ import { StepGuide } from './StepGuide';
 import { ProductStatistics } from './ProductStatistics';
 import { ProductAI } from './ProductAI';
 import { Button } from '../../../../shared/ui';
-
-/** (수정예정) 판매통계 데이터 불러오기-API */
-const DATA = {
-  reordered: [
-    { label: 0, count: 100 },
-    { label: 1, count: 50 },
-  ],
-  orderHour: [
-    { label: 0, count: 10 },
-    { label: 1, count: 30 }, // 생략...
-    { label: 23, count: 20 },
-  ],
-  orderDow: [
-    { label: 0, count: 20 },
-    { label: 1, count: 3 },
-    { label: 2, count: 34 },
-    { label: 3, count: 50 },
-    { label: 4, count: 14 },
-    { label: 5, count: 5 },
-    { label: 6, count: 30 },
-  ],
-  relatedProduct: [
-    // 20개 제공
-    {
-      productId: 0,
-      label: '상품1',
-      count: 10,
-    },
-    {
-      productId: 1,
-      label: 'Smart Ones',
-      count: 5,
-    },
-    {
-      productId: 2,
-      label: '상품11',
-      count: 15,
-    },
-    {
-      productId: 3,
-      label: '바나나',
-      count: 30,
-    },
-    {
-      productId: 3,
-      label: '딸기',
-      count: 30,
-    },
-    {
-      productId: 3,
-      label: '수박',
-      count: 10,
-    },
-  ],
-  report: '맛깔난 리포트',
-  period: {
-    max: '2025.05.20',
-    min: '2025.05.05',
-  },
-};
+import { useEffect, useState } from 'react';
+import { useGetProducts } from '../api/useGetProducts';
+import { StatisticsData } from '../type/StatisticsType';
+import Loading from '../../../../pages/Loading';
 
 export const ProductReport = () => {
+  const { isLoading, getProductsReport } = useGetProducts(); // 통계 데이터 API
+  const [data, setData] = useState<StatisticsData>(); // 통계 데이터
+
   const navigate = useNavigate();
   const [params] = useSearchParams();
   const productName = params.get('name');
@@ -80,6 +27,24 @@ export const ProductReport = () => {
     navigate(`/sponsor/report?${newParams}`);
   };
 
+  // 통계 데이터 불러오기 API
+  useEffect(() => {
+    const fetchReport = async () => {
+      const id = params.get('productId');
+      if (id) {
+        const result = await getProductsReport({ productId: Number(id) });
+        if (result) {
+          setData(result);
+        }
+      }
+    };
+    fetchReport();
+  }, [params]);
+
+  if (isLoading || !data) {
+    return <Loading description="통계데이터 불러오는중" />;
+  }
+
   return (
     <ContentWrapper>
       {/** 제품명 헤더 및 단계 */}
@@ -88,9 +53,9 @@ export const ProductReport = () => {
         <StepGuide />
       </HeaderWrapper>
       {/** 제품 최근 100건 판매 통계 */}
-      <ProductStatistics data={DATA} />
+      <ProductStatistics data={data} />
       {/** AI 리포트 */}
-      <ProductAI report={DATA.report} />
+      <ProductAI report={data.report} />
       {/** 다음 단계 버튼 */}
       <ButtonBox>
         <Button width="10rem" padding="1rem" textSize="1rem" onClick={handleNextStep}>
